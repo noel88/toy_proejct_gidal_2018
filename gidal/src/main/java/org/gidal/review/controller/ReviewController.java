9229ede2059289acf05d08c1517ffb839c11667c
noel88 <42628950+noel88@users.noreply.github.com>
2018-09-11 14:04:17 +0900

    tomcat 9.0 수정

---
+++ gidal/src/main/java/org/gidal/review/controller/ReviewController.java
@@ -0,0 +1,5 @@
+package org.gidal.review.controller;
+
+public class ReviewController {
+
+}
---
+++ gidal/src/test/java/org/system/web/DB_Test.java
@@ -0,0 +1,23 @@
+package org.system.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class DB_Test {
+
+
+
+	@Test
+	public void test() throws Exception {
+	    Class.forName("org.mariadb.jdbc.Driver"); // 마리아DB
+	    // Class.forName("com.mysql.jdbc.Driver"); MySQL
+
+	    Connection con = DriverManager.getConnection("jdbc:mariadb://127.0.0.1:3307/admin", "root", "root");// 마리아DB
+	    // Connection con =     DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","passwd"); MySQL
+	    System.out.println(con);
+	    }
+	}
+
+

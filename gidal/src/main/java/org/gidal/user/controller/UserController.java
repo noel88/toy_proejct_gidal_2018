b201708ee3635b9b0e6b6aa9281f66d8f9b31bb6
n <xiah0526@gmail.com>
2018-09-14 23:01:19 +0900

    성준 - user수정

--- gidal/src/main/java/org/gidal/user/controller/UserController.java
+++ gidal/src/main/java/org/gidal/user/controller/UserController.java
@@ -1,10 +1,45 @@
 package org.gidal.user.controller;
 
+import javax.inject.Inject;
+
+import org.gidal.enterprise.domain.EnterpriseVO;
+import org.gidal.enterprise.service.EnterpriseService;
+import org.gidal.user.domain.UserVO;
+import org.gidal.user.service.UserJoinService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/user/")
 public class UserController {
-//
+	@Inject
+	private UserJoinService service;
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public String user_joinForm() {
+
+		return "user/user";
+
+	}
+
+
+	@RequestMapping(value = "/userjoin", method = RequestMethod.POST)
+	public String user_join(UserVO vo, Model model) {
+		 int result = service.user_join(vo);
+
+		 if(result > 0) {
+			 model.addAttribute("id", vo);
+			 return "redirect:/";
+		 }else {
+			 //error
+			 return "redirect:/signFail";
+		 }
+	}
+
+	//수정
+
 
 
 

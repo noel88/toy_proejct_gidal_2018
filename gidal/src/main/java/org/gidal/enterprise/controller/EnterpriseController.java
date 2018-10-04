2456ace389cf356081de98440b39f3425a9987b0
noel88 <42628950+noel88@users.noreply.github.com>
2018-10-04 17:34:46 +0900

    TEST

--- gidal/src/main/java/org/gidal/enterprise/controller/EnterpriseController.java
+++ gidal/src/main/java/org/gidal/enterprise/controller/EnterpriseController.java
@@ -117,7 +117,10 @@ public class EnterpriseController {
 
 		EnterpriseVO vo = new EnterpriseVO();
 
-			//파일업로드
+		//파일업로드
+		long time = System.currentTimeMillis();
+		SimpleDateFormat dayTime = new SimpleDateFormat("yyyy-mm-dd");
+		String str = dayTime.format(new Date(time));
 
 			vo.setEnterprise_email(request.getParameter("enterprise_email"));
 			vo.setEnterprise_password(request.getParameter("enterprise_password"));
@@ -135,11 +138,11 @@ public class EnterpriseController {
 			vo.setEnterprise_sectors(request.getParameter("enterprise_sectors"));
 			vo.setEnterprise_service(request.getParameter("enterprise_service"));
 
-			MultipartFile mf = request.getFile("enterprise_mainImg");
-			MultipartFile mf1 = request.getFile("enterprise_img1");
-			MultipartFile mf2 = request.getFile("enterprise_img2");
-			MultipartFile mf3 = request.getFile("enterprise_img3");
-			MultipartFile mf4 = request.getFile("enterprise_img4");
+			MultipartFile mf = request.getFile(str+"_"+ vo.getEnterprise_code() +"_"+"enterprise_mainImg");
+			MultipartFile mf1 = request.getFile(str+"_"+ vo.getEnterprise_code() +"_"+"enterprise_img1");
+			MultipartFile mf2 = request.getFile(str+"_"+ vo.getEnterprise_code() +"_"+"enterprise_img2");
+			MultipartFile mf3 = request.getFile(str+"_"+ vo.getEnterprise_code()+"_"+"enterprise_img3");
+			MultipartFile mf4 = request.getFile(str+"_"+ vo.getEnterprise_code() +"_"+"enterprise_img4");
 
 			String filename = mf.getOriginalFilename();
 			String filename1 = mf1.getOriginalFilename();
@@ -147,11 +150,11 @@ public class EnterpriseController {
 			String filename3 = mf3.getOriginalFilename();
 			String filename4 = mf4.getOriginalFilename();
 
-			File uploadFile = new File("//var//webapps//upload//enterprise" + filename);
-			File uploadFile1 = new File("//var//webapps//upload//enterprise" + filename1);
-			File uploadFile2 = new File("//var//webapps//upload//enterprise" + filename2);
-			File uploadFile3 = new File("//var//webapps//upload//enterprise" + filename3);
-			File uploadFile4 = new File("//var//webapps//upload//enterprise" + filename4);
+			File uploadFile = new File("./var/webapps/upload/enterprise" + filename);
+			File uploadFile1 = new File("./var/webapps/upload/enterprise" + filename1);
+			File uploadFile2 = new File("./var/webapps/upload/enterprise" + filename2);
+			File uploadFile3 = new File("./var/webapps/upload/enterprise" + filename3);
+			File uploadFile4 = new File("./var/webapps/upload/enterprise" + filename4);
 
 			try {
 

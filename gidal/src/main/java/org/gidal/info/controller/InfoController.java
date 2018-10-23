ef9e5d75ef8d544e3baba5600876a845fd06d0be
tlqpffls <tlqpffls@naver.com>
2018-10-23 12:48:50 +0900

    민수 - 이벤트 주석 추가 및 불필요 코드 정리 - 소개 주석 추가 및 불필요 코드 정리

--- gidal/src/main/java/org/gidal/info/controller/InfoController.java
+++ gidal/src/main/java/org/gidal/info/controller/InfoController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/info")
 public class InfoController {
 
+	// '기달이란' 페이지 이동
 	@RequestMapping(value = "/whatIs", method = RequestMethod.GET)
 	public void whatIs() {
 	}
 
+	// 오시는길 페이지 이동
 	@RequestMapping(value = "/wayToCome", method = RequestMethod.GET)
 	public void wayToCome() {
 	}
